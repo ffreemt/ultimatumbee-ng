03ba4db29d67ee6e01923cc416142634d7b070f2
freemt <yucongo+fmt@gmail.com>
2022-02-24 01:59:56 +0800

    Update okteto.yml

---
+++ install-pytorch.sh
@@ -0,0 +1,8 @@
+# apt install -y python3-dev python3-venv python3-pip
+# apt install -y python3.9-dev python3.9-venv
+
+# pip install torch==1.8.2+cpu torchvision==0.9.2+cpu torchaudio==0.8.2 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
+
+wget -c https://download.pytorch.org/whl/cpu/torch-1.10.0%2Bcpu-cp39-cp39-linux_x86_64.whl
+
+# pip install https://download.pytorch.org/whl/cpu/torch-1.10.0%2Bcpu-cp310-cp310-linux_x86_64.whl
