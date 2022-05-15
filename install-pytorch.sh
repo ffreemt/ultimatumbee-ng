66984112ca37e82251970dcc42f732399c2ccc04
freemt <yucongo+fmt@gmail.com>
2022-05-16 00:34:32 +0800

    Update

--- install-pytorch.sh
+++ install-pytorch.sh
@@ -1,5 +1,8 @@
 # apt install -y python3-dev python3-venv python3-pip
 # apt install -y python3.9-dev python3.9-venv
+# python3.9 -m venv .venv
+# source .venv/bin/activate
+#  pip install poetry
 
 # pip install torch==1.8.2+cpu torchvision==0.9.2+cpu torchaudio==0.8.2 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
 
